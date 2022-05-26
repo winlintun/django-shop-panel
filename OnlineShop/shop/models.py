568cda40d9605a097f0822f7bf5917595b8e1d6c
iceface <www.iceface@gmail.com>
2022-05-26 20:53:00 +0630

    Customer register on Admin Panel

--- OnlineShop/shop/models.py
+++ OnlineShop/shop/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 # Create your models here.
 
 class Customer(models.Model):
-    name = models.CharField(max_length=200, help_text='Customer Name')
-    email = models.EmailField(help_text="Email")
+    name = models.CharField(max_length=200, help_text='Customer Name: Win Lin Tun')
+    email = models.EmailField(help_text="Email: netuser979@gmail.com")
     phone = models.CharField(max_length=100, help_text='Phone Number: 09-255458532')
     created_at = models.DateTimeField(default=timezone.now)
     
