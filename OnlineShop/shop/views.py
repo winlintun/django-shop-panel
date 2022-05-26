8645f7e55e908d80576525bf9ee67afe0621ff42
iceface <www.iceface@gmail.com>
2022-05-26 22:56:06 +0630

    dashboard django template

--- OnlineShop/shop/views.py
+++ OnlineShop/shop/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Customer, Product
+from .models import Customer, Product, Order
 # Create your views here.
 
 def home(request):
-    return render(request, 'shop/home.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    return render(request, 'shop/home.html', {
+        'customers': customers,
+        'orders': orders
+    })
 
 def customer(request):
     return render(request, 'shop/customer.html')
