f1f9e178994bb522a6ad67a3ef52be7d4c7e9bf5
iceface <www.iceface@gmail.com>
2022-05-26 20:49:02 +0630

    create Customer Model Class

--- OnlineShop/shop/models.py
+++ OnlineShop/shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, help_text='Customer Name')
+    email = models.EmailField(help_text="Email")
+    phone = models.CharField(max_length=100, help_text='Phone Number: 09-255458532')
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.name
+
