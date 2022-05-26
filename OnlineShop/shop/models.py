3b0498543468256ba13089cb12c112aa17828a5a
iceface <www.iceface@gmail.com>
2022-05-26 21:06:15 +0630

    create Tag Model Class

--- OnlineShop/shop/models.py
+++ OnlineShop/shop/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
 
 class Product(models.Model):
     category = (
