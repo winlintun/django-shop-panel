1ed958bb89d6d33df952f937675f659ddae6e9af
iceface <www.iceface@gmail.com>
2022-06-01 21:35:52 +0630

    filter date add function

--- OnlineShop/shop/filters.py
+++ OnlineShop/shop/filters.py
@@ -1,8 +1,11 @@
 from django_filters import FilterSet
 from .models import Order
+from django_filters import DateFilter
 
 
 class OrderFilter(FilterSet):
+    start_date = DateFilter(field_name='created_at', lookup_expr='gte')
+    end_date = DateFilter(field_name='created_at', lookup_expr='lte')
     class Meta:
         model = Order 
         fields = '__all__'
